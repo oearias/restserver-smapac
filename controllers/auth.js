89602a254cf63c4029bfc0d6740691d44d86d2d2
Oscar Arias <oscar.enrique.arias@gmail.com>
2021-10-16 10:14:55 -0500

    Se establece el mailverificationlink  con token como parametro

--- controllers/auth.js
+++ controllers/auth.js
@@ -85,7 +85,9 @@ const forgotPassword = async (req, res = response) => {
 
             const token = await genTokenPassword(id, email);
 
-            verificationLink = process.env.URL_API+`auth/new-password/${token}`;
+            //verificationLink = process.env.URL_API+`auth/new-password/${token}`;
+
+            verificationLink = 'http://smapac-client.s3-website.us-east-2.amazonaws.com/'+`reset?t=${token}`;
 
             //inserto en la tabla usuario colum resetToken el token
             const inserta = await pool
@@ -140,34 +142,7 @@ const forgotPassword = async (req, res = response) => {
                   emailMessage = "Mensaje enviado: "+info.response;
               }
           } );
-
-
-          /*let transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-              user: testAccount.user, // generated ethereal user
-              pass: testAccount.pass, // generated ethereal password
-            },
-          });
-        
-          // send mail with defined transport object
-          let info = await transporter.sendMail({
-            from: '"Fred Foo 👻" <foo@example.com>', // sender address
-            to: "bar@example.com, baz@example.com", // list of receivers
-            subject: "Hello ✔", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>", // html body
-          });
-        
-          console.log("Message sent: %s", info.messageId);
-          // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        
-          // Preview only available when sending through an Ethereal account
-          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));*/
          
-
 
     } catch (error) {
         return res.status(400).json({message: 'Algo ha ido mal'})
